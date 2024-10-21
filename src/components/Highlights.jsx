ad3ae5e5970576fe4aa79b77226cf2e65c18b979
Shaharyar Ahmed <shaharyar.ahmed000@gmail.com>
2024-10-21 16:29:57 +0500

    Video Carousel with progress highlight

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,13 +1,15 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { rightImg, watchImg } from "../utils";
+
 import VideoCarousel from "./VideoCarousel";
 
 const Highlights = () => {
   useGSAP(() => {
     gsap.to("#title", { opacity: 1, y: 0 });
-    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.2 });
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
   }, []);
+
   return (
     <section
       id="highlights"
@@ -16,8 +18,9 @@ const Highlights = () => {
       <div className="screen-max-width">
         <div className="mb-12 w-full md:flex items-end justify-between">
           <h1 id="title" className="section-heading">
-            Get the Highlights
+            Get the highlights.
           </h1>
+
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
               Watch the film
@@ -29,6 +32,7 @@ const Highlights = () => {
             </p>
           </div>
         </div>
+
         <VideoCarousel />
       </div>
     </section>

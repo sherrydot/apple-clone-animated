b2072ae54e4db6b79d4658b10a52f9ec06e5d2ca
Shaharyar Ahmed <69304334+sherrydot@users.noreply.github.com>
2024-10-27 21:36:39 +0000

    migration 1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">iPhone 15 Pro</p>
+        <p className="hero-title">iPhone 15 Pro.</p>
         <div className="md:w-10/12 w-9/12">
           <video
             className="pointer-events-none"
